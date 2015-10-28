5d9dd50f46f5459d31011c2a0ab1ca17cc4d3738
Matt Thomson <red.cataclysm@gmail.com>
2015-10-28 01:42:43 -0400

    Refactor: Show Recap Sub-section

---
+++ app/public/javascript/show-recap.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('paper-icon-button.recap').on('click', function(e) {
+    var btn = $(e.currentTarget);
+    var game = btn.parent().parent().parent();
+    var header = game.children('.header');
+    var recap = game.children('.recap');
+
+    btn.attr('icon') === 'expand-more' ?
+        btn.attr('icon', 'expand-less') :
+        btn.attr('icon', 'expand-more');
+
+    header.attr('elevation') == 0 ?
+        header.attr('elevation', 3) :
+        header.attr('elevation', 0);
+    recap.slideToggle();
+  });
+});
