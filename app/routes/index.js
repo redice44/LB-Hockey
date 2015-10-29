84a0e8f4896cf46f9041dcfc2b131e82637ae697
Matt Thomson <red.cataclysm@gmail.com>
2015-10-29 12:48:13 -0400

    Implement: Basic Routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,17 @@
 module.exports = exports = function(app) {
   app.get('/', function(req, res, next) {
-    res.render('schedule');
+    res.render('schedule', {section: 0});
+  });
+
+  app.get('/schedule', function(req, res, next) {
+    res.render('schedule', {section: 0});
+  });
+
+  app.get('/standings', function(req, res, next) {
+    res.render('standings', {section: 1});
+  });
+
+  app.get('/roster', function(req, res, next) {
+    res.render('roster', {section: 2});
   });
 };
